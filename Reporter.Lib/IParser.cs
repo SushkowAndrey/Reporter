8ed79d76bacdb425e8f5e689e4c9126d356e42c5
Starinin Andrey <starininandrey@gmail.com>
2021-12-26 13:33:45 +0300

    Add JsonParser

--- Reporter.Lib/IParser.cs
+++ Reporter.Lib/IParser.cs
@@ -3,6 +3,6 @@ namespace Reporter.Lib
 {
     public interface IParser
     {
-        public Dom.Dom Parsing();
+        public Dom.Dom Parsing(string str);
     }
 }
---
+++ Reporter.Lib/JsonParser.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using Reporter.Lib.Dom;
+
+namespace Reporter.Lib
+{
+    public class JsonParser : IParser
+    {
+        public Dom.Dom Parsing(string str)
+        {
+            var dom = new Dom.Dom();
+            
+            var jsonDoc = JsonDocument.Parse(str);
+            var root = jsonDoc.RootElement;
+
+            var elements = root.EnumerateObject();
+            foreach (var element in elements)
+            {
+                dom.Add(element.Name, element.Value.GetString());
+            }
+
+            return dom;
+        }
+    }
+}
