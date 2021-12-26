07c105d945c3d8a447934f9b9cf2ac0483698186
Starinin Andrey <starininandrey@gmail.com>
2021-12-26 12:46:03 +0300

    Init Reporter.Lib.Dom

---
+++ Reporter.Lib.Dom/Dom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Reporter.Lib.Dom
+{
+    public class Dom
+    {
+        private List<Element> _dom;
+
+        public Dom()
+        {
+            _dom = new List<Element>();
+        }
+
+        public void Add(string key, string value)
+        {
+            _dom.Add(new Element
+            {
+                Key = key,
+                Value = value
+            });
+        }
+    }
+}
---
+++ Reporter.Lib.Dom/Element.cs
@@ -0,0 +1,11 @@
+﻿namespace Reporter.Lib.Dom
+{
+    public record Element
+    {
+        public string Key { get; init; }
+        public string Value { get; init; }
+
+        public void Deconstruct(out string elementKey, out string elementValue) =>
+            (elementKey, elementValue) = (Key, Value);
+    }
+}
