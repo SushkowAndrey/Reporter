6e5737591ce2e1eacae3532897a3ca024f83c215
Starinin Andrey <starininandrey@gmail.com>
2021-12-26 14:17:28 +0300

    Init test app

---
+++ Reporter.TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Reporter.Lib;
+
+namespace Reporter.TestApp
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            using var input = new StreamReader("test.json");
+            var json = input.ReadToEnd();
+
+            IParser parser = new JsonParser();
+            IGenerator generator = new GeneratorHtml();
+
+            var html = generator.Generator(parser.Parsing(json));
+
+            using var output = new StreamWriter("test.html", append: false);
+            output.Write(html);
+        }
+    }
+}
